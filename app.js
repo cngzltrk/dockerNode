61c59f785ced2390d8b6a173da5456ab6cff0d8f
Cengiz Alturk <cengiz.alturk@n11.com>
2022-02-03 21:51:07 +0300

    some change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 80;
 // // App
 const app = express();
 app.get('/', (req, res) => {
-   res.send('Hello world\n');
+   res.send('Hello world\n Hello world\n Hello world\n');
 console.log(`tehere is q request`);   
 });
 
